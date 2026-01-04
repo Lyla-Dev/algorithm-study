80e84d229403a0f1ac98f2d1bd77d57c22efaeb0
heayoooon <heayoon381@gmail.com>
2026-01-04 17:45:35 +0900

    edit Day02-1

--- EntryLevel/Day2/1_heayoon.cpp
+++ EntryLevel/Day2/1_heayoon.cpp
@@ -5,11 +5,4 @@ using namespace std;
 
 int solution(int num1, int num2) {
     return static_cast<int>((static_cast<double>(num1) / num2) * 1000);
-}#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(int num1, int num2) {
-    return static_cast<int>((static_cast<double>(num1) / num2) * 1000);
 }
