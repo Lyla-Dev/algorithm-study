937b1bc10a77d6f8f22de491c983c108545d33f5
Taeppir <wanghope@dgu.ac.kr>
2026-01-06 18:15:55 +0900

    solved #3

--- EntryLevel/Day2/3_Taehee.py
+++ EntryLevel/Day2/3_Taehee.py
@@ -1,11 +1,19 @@
+# 최대공약수
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
+
+    # 분모 통분 후 분자 계산
     denom = denom1 * denom2
-    numer = numer1 * (denom//denom1) + numer2 * (denom//denom2)
+    numer = numer1 * denom2 + numer2 * denom1
+
+    # 약분
+    common = gcd(numer, denom)
     
-    answer.append(numer)
-    answer.append(denom)
-    return answer
+    return [numer // common, denom // common] 
 
 print(solution(1,2,3,4))
 print(solution(9,2,1,3))
