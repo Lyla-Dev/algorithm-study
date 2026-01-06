1a991015cd92414499a75c5c099c5d2daaec7c8c
Taeppir <wanghope@dgu.ac.kr>
2026-01-06 18:38:19 +0900

    #4: list comprehension

--- EntryLevel/Day2/4_Taehee.py
+++ EntryLevel/Day2/4_Taehee.py
@@ -1,10 +1,13 @@
+# def solution(numbers):
+#     answer = []
+
+#     for num in numbers:
+#         answer.append(2 * num)
+
+#     return answer
+
 def solution(numbers):
-    answer = []
-
-    for num in numbers:
-        answer.append(2 * num)
-
-    return answer
+    return [num * 2 for num in numbers]
 
 print(solution([1,2,3,4,5]))
 print(solution([1,2,100,-98,1,2,3]))
