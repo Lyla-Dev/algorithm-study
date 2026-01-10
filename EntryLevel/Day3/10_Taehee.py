e1bcfaae12b41e499a08c38cbf2a0e0230fef338
Taeppir <wanghope@dgu.ac.kr>
2026-01-11 01:19:22 +0900

    solve: #12 and add differnt solutions

--- EntryLevel/Day3/10_Taehee.py
+++ EntryLevel/Day3/10_Taehee.py
@@ -10,11 +10,11 @@ def solution(array):
 print(solution([1,2,7,10,11]))
 print(solution([9,-1,0]))
 
-"""
-[sort()와 sorted() 차이]
-- list.sort(): 리스트 원본 직접 정렬
-- sorted(list): 원본 건드리지 않고, 정렬된 새로운 리스트 만들어 반환
-"""
+# sort()와 sorted() 차이
+# - list.sort(): 리스트 원본 직접 정렬
+# - sorted(list): 원본 건드리지 않고, 정렬된 새로운 리스트 만들어 반환
+
+'''
 arr1 = [3, 1, 2]
 result = arr1.sort()
 print(arr1, result)
@@ -24,4 +24,11 @@ arr2 = [3, 1, 2]
 result2 = sorted(arr2)
 print(arr2, result2)
 # [3, 1, 2] [1, 2, 3]
+'''
 
+# 다른 풀이 -----------------------------
+def solution2(array):
+    return sorted(array)[len(array)//2]
+
+print(solution2([1,2,7,10,11]))
+print(solution2([9,-1,0]))
--- EntryLevel/Day3/12_Taehee.py
+++ EntryLevel/Day3/12_Taehee.py
@@ -1,11 +1,31 @@
-def solution(num1, num2):
-    return num1 % num2
+def solution(n):
+    results = []
+    i = 0
+    while i <= n:
+        if i % 2 == 1:
+            results.append(i)
+        i += 1
+    return results
 
-print(solution(3,2))
-print(solution(10,5))
+print(solution(10))       
+print(solution(15))
 
-# 다른 풀이 - 뺄셈으로 접근
-def solution2(num1, num2):
-    while num1 >= num2:
-        num1 -= num2
-    return num1
+# range(시작, 끝, 보폭)
+'''
+for i in range(1, n+1, 2):
+    results.append(i)
+'''
+
+# 다른 풀이: 리스트 컴프리헨션
+def solution2(n):
+    return [i for i in range(1, n+1, 2)]
+
+print(solution2(10))       
+print(solution2(15))
+
+# 다른 풀이: list()로 바로 변환
+def solution3(n):
+    return list(range(1, n+1, 2))
+
+print(solution3(10))       
+print(solution3(15))
