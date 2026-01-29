92fbdee075f7890e4a8dc91d9278bf14c0bd0792
Taeppir <wanghope@dgu.ac.kr>
2026-01-29 23:51:45 +0900

    solve: day12

---
+++ EntryLevel/Day12/45_Taehee.py
@@ -0,0 +1,12 @@
+def solutioin(my_string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    for c in vowels:
+        my_string = my_string.replace(c, '')
+
+    return my_string
+
+# 다른 풀이
+# 'aeiou'에 없는 글자만 뽑아서 리스트로 만들고, 다시 조인('')
+def solution(my_string):
+    return "".join([char for char in my_string if char not in "aeiou"])
