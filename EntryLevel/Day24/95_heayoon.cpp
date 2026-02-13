0e72c15d7f2323f62cbeeac9de4497c05aadd727
heayoooon <heayoon381@gmail.com>
2026-02-14 02:43:53 +0900

    95 수정

--- EntryLevel/Day24/95_heayoon.cpp
+++ EntryLevel/Day24/95_heayoon.cpp
@@ -10,3 +10,18 @@ int solution(string before, string after) {
     if(before == after) return 1;
     return 0;
 }
+// O(nlogn)
+
+int solution(string before, string after) {
+    vector<int> cnt(26, 0);                 // 알파벳 저장 벡터
+
+    for (char c : before) cnt[c - 'a']++;
+    for (char c : after) cnt[c - 'a']--;
+
+    for (int n : cnt){
+        if (n != 0) return 0;
+    }
+
+    return 1;
+}
+// O(n)
